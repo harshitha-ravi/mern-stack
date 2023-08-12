beac481efa384b28c3389b9abc2e49b655f1d9c6
Harshitha Ravi <hersheyvince07@gmail.com>
2023-08-11 22:14:54 -0500

    app.js modification

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,14 @@
 import { BrowserRouter, Navigate, Routes, Route } from "react-router-dom";
 import HomePage from "views/homePage";
 import ProfilePage from "views/profilePage";
-import LoginPage from "views/loginPage";  
+import LoginPage from "views/loginPage";
 
-// imports realted to theme settings 
+// imports realted to theme settings
 import { useMemo } from "react";
 import { useSelector } from "react-redux";
 import { CssBaseline, ThemeProvider } from "@mui/material";
 import { createTheme } from "@mui/material/styles";
 import { themeSettings } from "./theme";
-
 
 function App() {
   // This is how we grab the gloab state defined in initialState in state -> index.js
@@ -19,21 +18,27 @@ function App() {
   // To set up the theme -> this is passed to theme provider
   const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
 
-
+  // grab the auth info
+  const isAuth = Boolean(useSelector((state) => state.token));
 
   return (
     <div className="app">
       <BrowserRouter>
-      <ThemeProvider theme={theme}>
-        <CssBaseline />
-       <Routes>
-         <Route path="/" element={<LoginPage/>} />
-         <Route path="/home" element={<HomePage/>} />
-         <Route path="/profile/:studentId" element={<ProfilePage/>} />
-       </Routes>
-       </ThemeProvider>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <Routes>
+            <Route path="/" element={<LoginPage />} />
+            <Route
+              path="/home"
+              element={isAuth ? <HomePage /> : <Navigate to="/" />}
+            />
+            <Route
+              path="/profile/:studentId"
+              element={isAuth ? <ProfilePage /> : <Navigate to="/" />}
+            />
+          </Routes>
+        </ThemeProvider>
       </BrowserRouter>
-      
     </div>
   );
 }
