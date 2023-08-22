07d340c1d743958eb23002f25af08968f048afc6
hxr8557 <hersheyvince07@gmail.com>
2023-08-21 23:27:48 -0500

    Updates

--- server/routes/post.js
+++ server/routes/post.js
@@ -1,14 +1,18 @@
 import express from "express";
-import { getFeedQuestions, getStudentQuestions, upVote } from "../controller/post.js";
+import {
+  getFeedQuestions,
+  getStudentQuestions,
+  upVote,
+} from "../controller/post.js";
 import { verifyToken } from "../middleware/auth.js";
 
 const router = express.Router();
 
 /** READ */
 router.get("/", verifyToken, getFeedQuestions);
-router.get("/:studentId/posts", verifyToken, getStudentQuestions);
+router.get("/:studentId", verifyToken, getStudentQuestions);
 
 /** UPDATE */
-router.patch("/upVote/:id", verifyToken, upVote);
+router.patch("/upVote/:postId", verifyToken, upVote);
 
 export default router;
